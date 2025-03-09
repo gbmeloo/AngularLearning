50c0ea15c3d98d978799b19bbbd5fd99b8a016e3
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-03-09 10:33:40 -0700

    Added a propper image slider

--- week1/router/src/app/components/home/home.component.ts
+++ week1/router/src/app/components/home/home.component.ts
@@ -8,5 +8,14 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  images = [1, 2, 3].map(() => `https://picsum.photos/600/400?random&t=${Math.random()}`);
+  images = [1, 2, 3, 4].map(() => `https://picsum.photos/600/400?random&t=${Math.random()}`);
+  currentIndex = 0;
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
